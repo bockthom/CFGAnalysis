fe3312bfcfbe1dd8dbc0c74fc1caa97b7e024f3d
bockthom <bockthom@fim.uni-passau.de>
2015-04-25 21:08:55 +0200

    Fixed some bugs in CFGValidator

--- src/main/scala/CFGValidator.scala
+++ src/main/scala/CFGValidator.scala
@@ -1,6 +1,7 @@
 package de.fosd.typechef.cfganalysis
 
 import de.fosd.typechef.options.{FrontendOptionsWithConfigFiles, FrontendOptions, OptionException}
+import de.fosd.typechef.options._
 
 class CFGValidator { 
 
@@ -9,7 +10,7 @@ class CFGValidator {
         // load options
         val opt = new FrontendOptionsWithConfigFiles()
         try {
-		try {
+		try {			
 			opt.parseOptions(args)
 		} catch {
 			case o: OptionException => if (!opt.isPrintVersion) throw o
@@ -17,21 +18,32 @@ class CFGValidator {
    	}
    	catch {
 		case o: OptionException =>
-		println("Invocation error: " + o.getMessage)
-		println("use parameter --help for more information.")
-		return cfg
+		if(! o.getMessage.equals("No file specified.") 
+		   && ! o.getMessage.equals("Multiple files specified. Only one supported.")) {
+			println("Invocation error: " + o.getMessage)
+			println("use parameter --help for more information.")
+			return cfg
+		}
     	}
 
 	// get feature model
     	val fm = opt.getFullFeatureModel
 
-	// validate against the feature model
+	// validate nodes against the feature model
     	val satNodes = cfg.nodes.filter(_.fexpr.isSatisfiable(fm))
-    	println("Nodes (sat/unsat): " + satNodes.size + " vs. " + cfg.nodes.size)
+    	println("Nodes (sat/all): " + satNodes.size + " vs. " + cfg.nodes.size)
 
+	// validate edges against the feature model
     	val satEdges = cfg.edges.filter(_._3.isSatisfiable(fm))
-   	println("Edges (sat/unsat): " + satEdges.size + " vs. " + cfg.edges.size)
+   	println("Edges (sat/all): " + satEdges.size + " vs. " + cfg.edges.size)
 
-	return (new CFG(satNodes,satEdges))
+	// remove edges which source or target was removed in validation step above
+	val unsatNodes = cfg.nodes.filterNot(_.fexpr.isSatisfiable(fm))
+	val unsatNodeIds:Set[Int] = unsatNodes.map(_.id)
+	val satEdgesWithoutUnsatSourceNodes = satEdges.filterNot(x => unsatNodeIds.contains(x._1.id))
+	val satEdgesWithoutUnsatNodes = satEdgesWithoutUnsatSourceNodes.filterNot(x => unsatNodeIds.contains(x._2.id))
+	println("Edges (sat without unsat. nodes/all): " + satEdgesWithoutUnsatNodes.size + " vs. " + satEdges.size)
+
+	return (new CFG(satNodes,satEdgesWithoutUnsatNodes))
     }
 }
