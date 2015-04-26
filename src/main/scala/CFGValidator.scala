80d8636e0b317a5e1712df07da157a3559a276f2
bockthom <bockthom@fim.uni-passau.de>
2015-04-26 13:39:45 +0200

    Adjusted output text in CFGValidator

--- src/main/scala/CFGValidator.scala
+++ src/main/scala/CFGValidator.scala
@@ -31,18 +31,19 @@ class CFGValidator {
 
 	// validate nodes against the feature model
     	val satNodes = cfg.nodes.filter(_.fexpr.isSatisfiable(fm))
-    	println("Nodes (sat/all): " + satNodes.size + " vs. " + cfg.nodes.size)
+    	println("Nodes (sat/unsat): " + satNodes.size + " vs. " + cfg.nodes.size)
 
 	// validate edges against the feature model
     	val satEdges = cfg.edges.filter(_._3.isSatisfiable(fm))
-   	println("Edges (sat/all): " + satEdges.size + " vs. " + cfg.edges.size)
+   	println("Edges (sat/unsat): " + satEdges.size + " vs. " + cfg.edges.size)
 
 	// remove edges which source or target was removed in validation step above
 	val unsatNodes = cfg.nodes.filterNot(_.fexpr.isSatisfiable(fm))
 	val unsatNodeIds:Set[Int] = unsatNodes.map(_.id)
 	val satEdgesWithoutUnsatSourceNodes = satEdges.filterNot(x => unsatNodeIds.contains(x._1.id))
 	val satEdgesWithoutUnsatNodes = satEdgesWithoutUnsatSourceNodes.filterNot(x => unsatNodeIds.contains(x._2.id))
-	println("Edges (sat without unsat. nodes/all): " + satEdgesWithoutUnsatNodes.size + " vs. " + satEdges.size)
+	println("SatEdges (after/before removing edges which source or target node is unsat): " 
+		+ satEdgesWithoutUnsatNodes.size + " vs. " + satEdges.size)
 
 	return (new CFG(satNodes,satEdgesWithoutUnsatNodes))
     }
